0ee9ad9df881549b857d1ef28ec41628c15615ff
rishi0810 <rishi.1003raj@gmail.com>
2025-07-08 11:57:27 +0530

    update middleware

--- LoggingMiddleware/logFunction.js
+++ LoggingMiddleware/logFunction.js
@@ -1,9 +1,10 @@
-const log = async()=>{
+const log = async(body)=>{
     const response = await fetch("http://20.244.56.144/evaluation-service/logs", {
         method : "POST",
         headers : {
             "Content-Type" : "application/json",
             "Authorization" : `Bearer ${token}`
-        }
+        },
+        body : JSON.stringify(body)
     })
 }
